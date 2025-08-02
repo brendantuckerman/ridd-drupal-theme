0bd58c759f2db6a0846cdfc2e1760149313ec2a1
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2025-08-02 14:07:31 +1000

    Color vars

--- src/stories/atoms/Button.stories.js
+++ src/stories/atoms/Button.stories.js
@@ -1,28 +1,30 @@
-import { fn } from 'storybook/test';
 
-import { createButton } from './Button';
+import '../../../components/atoms/button/button.css';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
-  title: 'Example/Button',
+  title: 'Ridd/Button',
   tags: ['autodocs'],
-  render: ({ label, ...args }) => {
+  render: ({ text }) => {
     // You can either use a function to create DOM elements or use a plain html string!
-    // return `<div>${label}</div>`;
-    return createButton({ label, ...args });
+    return `<button class="ridd-btn"></button>`;
   },
   argTypes: {
-    backgroundColor: { control: 'color' },
-    label: { control: 'text' },
-    onClick: { action: 'onClick' },
-    primary: { control: 'boolean' },
+    text: { control: 'text'},
+    url:  { control: 'text'},
+    variant: { 
+      control: {type: 'select'},
+      options: ['primary', 'secondary']
+
+    },
+    disabled: { control: 'bool'},
     size: {
       control: { type: 'select' },
       options: ['small', 'medium', 'large'],
     },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
